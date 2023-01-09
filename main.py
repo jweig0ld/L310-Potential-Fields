380f681c69a0025ada09787b5b12ae77375a9021
jweig0ld <jamie.weigold.19@ucl.ac.uk>
2023-01-09 15:05:15 +0000

    Environment grid and planets visualising correctly

--- main.py
+++ main.py
@@ -2,20 +2,26 @@ import numpy as np
 
 from components import *
 from potential_field_navigator import *
+from visualisation import plot_env
 
 
 if __name__ == "__main__":
-    s_1 = Spaceship(np.array([-0.5, -0.5, -0.5]), 0.05, np.array([0., 0., 0.]))
-    spaceships = [s_1]
+    # s_1 = Spaceship(np.array([-0.5, -0.5, -0.5]), 0.05, np.array([0., 0., 0.]))
+    # spaceships = [s_1]
 
     # a_1 = Asteroid(np.array([]))
     # asteroids = [a_1]
 
-    # p_1 = Planet()
-    # planets = [p_1]
+    p1 = Planet(np.array([-1.5, 0.5, 0.5]), 1., 0)
+    p2 = Planet(np.array([2., 2., 0.]), 1., 0)
 
+    xlen, ylen, zlen = 8, 8, 3
+    spaceships = []
+    asteroids = []
+    planets = [p1, p2]
     dt, max_t = 10, 1000
-    
-    navigator = PotentialFieldNavigator()
+    navigator = None
 
-    env = Environment(2, 2, spaceships, asteroids, planets, dt, max_t, navigator)
+    env = Environment(xlen, ylen, zlen, spaceships, asteroids, planets, dt, max_t, navigator)
+    plot_env(env, z=0, filename='test.png')
+    
--- visualisation.py
+++ visualisation.py
@@ -4,21 +4,83 @@ import numpy as np
 from components import *
 
 
-def generate_2d_quiver(env, spaceship_idx, filename=None):
+RESOLUTION = 25
+GRID_OFFSET = 0.5
+
+
+"""
+TODO:
+
+1. (DONE). Plot the grid in 2D at z=0. 
+2. (DONE). Plot the planets on the grid in 2D, where z=0.
+3. Plot the asteroids on the grid in 2D, where z=0.
+4. Plot the trajectories of the asteroids on the grid in 2D, where z=0.
+5. Plot the spaceships on the grid in 2D, where z=0.
+6. Plot the trajectories of the spaceships on the grid, where z=0.
+7. Plot the vector field of a spaceship where z=0.
+8. Allow for the specification of a z value that is not zero. 
+"""
+
+
+def find_circle(c, r, z=0):
+    """
+    Returns the centre and radius of the circle that results from the
+    intersection of the plane z=k and the sphere with centre `c` and 
+    radius `r`.
+    """
+    centre = c.copy()
+    centre[2] = z
+    d = z - c[2] # Distance between centre of resultant circle and plane z=k.
+    radius = np.sqrt(np.square(r) - np.square(d))
+    return centre, radius
+
+
+def plot_env(env, z=0, filename=None):
+    # Walls 
+    plt.plot([-env.xlen/2, env.xlen/2], [-env.ylen/2, -env.ylen/2], 'k') # Bottom
+    plt.plot([-env.xlen/2, -env.xlen/2], [-env.ylen/2, env.ylen/2], 'k') # Left
+    plt.plot([env.xlen/2, env.xlen/2], [-env.ylen/2, env.ylen/2], 'k') # Right
+    plt.plot([-env.xlen/2, env.xlen/2], [env.ylen/2, env.ylen/2], 'k') # Top
+    plt.axis('equal')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.xlim([-env.xlen/2 - GRID_OFFSET, env.xlen/2 + GRID_OFFSET])
+    plt.ylim([-env.ylen/2 - GRID_OFFSET, env.ylen/2 + GRID_OFFSET])
+
+    # Planets
+    for planet in env.planets:
+        centre, radius = find_circle(planet.position, planet.radius, z)
+
+        # Draw planets in red
+        xs = np.linspace(0., 2 * np.pi, RESOLUTION)
+        x = np.cos(xs) * radius + centre[0]
+        y = np.sin(xs) * radius + centre[1]
+        plt.plot(x, y, 'r')
+
+    if filename:
+        plt.savefig(filename)
+
+
+def quiver_2d(env, navigator, spaceship_idx, filename=None):
     """
     Given the current location of a series of planets, asteroids and
     spaceships (and the velocities of the asteroids and spaceships),
     produce a 2D output illustrating the vector field associated with
     one or more spaceships.
     """
-    pass
 
+    # Potential Field Plotting
+    x = np.linspace(-env.xlen/2, env.xlen/2, RESOLUTION)
+    y = np.linspace(-env.ylen/2, env.ylen/2, RESOLUTION)
+    X, Y = np.meshgrid(x, y)
+    U, V = np.zeros_like(X), np.zeros_like(Y)
 
-def generate_3d_quiver():
-    """
-    Given the current location of a series of planets, asteroids and
-    spaceships (and the velocities of the asteroids and spaceships),
-    produce a 3D output illustrating the vector field associated with
-    one or more spaceships.
-    """
-    pass
+    for i in range(len(U)):
+        for j in range(len(V)):
+            # velocity = navigator.vector(env, spaceship_idx)
+            velocity = np.array([1., 1.])
+            U[i, j] = velocity[0]
+            V[i, j] = velocity[1]
+
+    plt.quiver(X, Y, U, V, units='width')
+    plt.show()
