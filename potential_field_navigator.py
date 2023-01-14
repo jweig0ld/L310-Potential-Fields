9ca83b9f5c7aea5986b5b74a93709af376b4ccd4
jweig0ld <jamie.weigold.19@ucl.ac.uk>
2023-01-14 20:44:30 +0000

    Refactored to include math calls

--- potential_field_navigator.py
+++ potential_field_navigator.py
@@ -5,17 +5,17 @@ from math_utils import normalise
 
 class PotentialFieldNavigator():
     def repulsive(self, env, spaceship_idx):
-        pass
+        return np.zeros((3,))
     
     def attractive(self, env, spaceship_idx):
         cur_pos = env._spaceship_trajectories[spaceship_idx][-1]
         direction = env.spaceships[spaceship_idx].goal - cur_pos
-        return self._normalise(direction)
+        return normalise(direction)
     
     def vector(self, env, spaceship_idx):
         """
         Ensure that the vectors are normalised or clipped when
         returned to prevent crazy spaceship trajectories.
         """
-        # return self.attractive(env, spaceship_idx) + self.repulsive(env, spaceship_idx)
-        return np.array([0.1, 0.1, 0])
+        return self.attractive(env, spaceship_idx) + self.repulsive(env, spaceship_idx)
+        # return np.array([0.1, 0.1, 0])
